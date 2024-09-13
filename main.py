dbc67553f3f078261d39adabeadcfdfcf6ef79a6
harshitakumai-dev <harshitakumari344@gmail.com>
2024-09-13 13:11:57 +0530

    l

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import validators
 import dns.resolver
 from fastapi.middleware.cors import CORSMiddleware
 import asyncio
-
-
+import os
 app = FastAPI()
 
 origins = [
@@ -94,4 +93,6 @@ async def main(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+ 
