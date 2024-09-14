859eb3b2a99e41750e703b1a400026c66c258631
harshitakumai-dev <harshitakumari344@gmail.com>
2024-09-14 20:33:46 +0530

    hqqq

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 origins = [
     # "https://email-validation-fr.vercel.app"
-    "https://email-val.netlify.app"
+    # "https://email-val.netlify.app/"
+    "https://email-validation-90.pages.dev/"
 ]
 
 app.add_middleware(
