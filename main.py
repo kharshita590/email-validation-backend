e52f48c718aad72c56f9bfb633e848a2897fa36a
harshitakumai-dev <harshitakumari344@gmail.com>
2024-09-14 10:21:16 +0530

    hiiiiiii

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all origins, or specify certain domains
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
 mx_cache = {}
