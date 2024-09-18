f0d88eb75ba3cf6c4141ecea4f0d78172f1088a3
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2024-09-19 01:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,6 @@ async def main(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
  
