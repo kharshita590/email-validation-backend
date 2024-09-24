24421949f09c0e439dd9779a2d37085c2b393d35
harshitakumai-dev <harshitakumari344@gmail.com>
2024-09-25 00:41:18 +0530

    iiiilkklk

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ async def main(file: UploadFile = File(...)):
 
         emails = df['email'].tolist()
         logger.info(f"Validating emails: {emails}")
-        batch_size = 25
+        batch_size = 5
         results = []
 
         for i in range(0, len(emails), batch_size):
