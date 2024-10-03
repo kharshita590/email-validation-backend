3de0163990c7405916604a5c52951933d8d40791
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2024-10-03 13:38:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -201,9 +201,9 @@ async def verify_email_sync(email):
        
 
     try:
-        async with SMTP(hostname=mx_host,port=587,tls_context=ssl_context,timeout=40,use_tls=True) as server:
+        async with SMTP(hostname=mx_host,port=465,tls_context=ssl_context,timeout=40,use_tls=True) as server:
             await asyncio.wait_for(server.connect(), timeout=40)
-            await server.starttls(ssl_context=ssl_context)
+            # await server.starttls(ssl_context=ssl_context)
             # await server.helo()
             # await server.connect()
             await server.mail("n640o@rustyload.com")
